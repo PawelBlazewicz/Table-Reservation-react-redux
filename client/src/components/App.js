c203fa1c434d93440dd35bdbbd63cab36d6fb3a6
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2019-12-22 17:23:27 +0100

    Navbar component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Main from './Main.js';
+import Navbar from './Navbar.js'
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/" component={Main} />
+        <div>
+          <Route path="/" component={Navbar} />
+          <Route path="/" exact component={Main} />
+        </div>
       </BrowserRouter>
     </div>
   );
+};
+
+const Navbar = () => {
+  return <div>nav</div>;
 };
 
 const Main = () => {
