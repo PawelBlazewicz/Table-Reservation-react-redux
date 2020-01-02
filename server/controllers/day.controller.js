5e8fd9d17e1abad5f9b7fe2c9413026eff27f372
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2020-01-02 19:23:35 +0100

    Adding reservation routing

--- server/controllers/day.controller.js
+++ server/controllers/day.controller.js
@@ -1,6 +1,9 @@
 const Day = require("../models/day.model.js");
+const mongoose = require("mongoose");
+const TableSchema = require("../models/table.model.js");
+const Table = mongoose.model("Table", TableSchema);
 
-exports.add_day = function(req, res) {
+exports.addDay = function(req, res) {
   let day = new Day({
     date: req.body.date,
   });
@@ -11,4 +14,34 @@ exports.add_day = function(req, res) {
     }
     res.send("New day Created successfully");
   });
+};
+
+exports.getReservations = function(req, res) {
+  Day.find({ date: req.body.day })
+    .exec()
+    .then(day => {
+      res.send(day);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.addReservations = function(req, res) {
+  Day.find({ date: req.body.day })
+    .exec()
+    .then(day => {
+      const table = new Table({
+        time: req.body.time,
+        table: req.body.table,
+        email: req.body.email,
+        message: req.body.message,
+      });
+      day[0].reservations.push(table);
+      day[0].save();
+      res.send("Reservations done");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
--- server/routes/day.route.js
+++ server/routes/day.route.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/day.controller.js");
 
+router.post('/day', controller.addDay);
 
-router.post('/day', controller.add_day);
+router.get('/reservations/', controller.getReservations);
+
+router.post('/reservations/', controller.addReservations);
 
 module.exports = router;
