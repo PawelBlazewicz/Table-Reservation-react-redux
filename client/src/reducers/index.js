ca1b2649518f86775ffce28c360b2f4dd5c4667a
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2020-01-05 22:43:27 +0100

    Refactor

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer, combineForms  } from 'react-redux-form';
+import { combineForms } from 'react-redux-form';
 import authReducer from './authReducer.js';
-import moment from 'moment';
-
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer,
-  forms: combineForms({
-    choosenDate: moment().format('LL'),
-  },'forms'),
+  forms: combineForms(
+    {
+      choosenDate: '',
+    },
+    'forms',
+  ),
 });
