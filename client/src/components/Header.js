2f282bb75e50a3be30d7189bde45912702ad7ace
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2020-01-05 23:16:29 +0100

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar.js';
 import './Header.css';
 
@@ -11,7 +12,9 @@ const Header = () => {
       <div id="header-text" className="ui text container">
         <h1 className="ui inverted header">Karczma Siedem Kotów</h1>
         <h2>Najlepsze jadło i trunki w mieście</h2>
+        <Link className="item" to="/Reservation" onClick={this.onItemClick}>
         <div className="ui inverted huge button">Zarezerwuj stolik</div>
+        </Link>
       </div>
     </div>
   );
